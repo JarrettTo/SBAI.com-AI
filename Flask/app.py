658780b89ce720b88ff150f70eb36a25afb10511
Justin To <104493864+JarrettTo@users.noreply.github.com>
2024-03-19 23:40:40 +0800

    Added threading and logging

--- Flask/app.py
+++ Flask/app.py
@@ -4,14 +4,20 @@ from datetime import date
 import json
 from flask import Flask, render_template, request
 from functools import lru_cache
-
+from threading import Thread
+import logging
 
 main_py_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'main.py'))
 sys.path.append(os.path.dirname(main_py_path))
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 from main import server_predict, train_models, update_data
 
 app = Flask(__name__)
+def update_data_background():
+    logging.info("Starting update_data task")
+    update_data()
+    logging.info("update_data task finished")
+
 
 @app.route("/")
 def index():
@@ -33,8 +39,9 @@ def train():
 
 @app.route("/update_data") 
 def update_data_route():
-    res1 = update_data()
-    return res1
+    thread = Thread(target=update_data_background)
+    thread.start()
+    return 'Update in progress'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
